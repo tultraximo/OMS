bd51f43571bafe2d13ec916068eea59e857033d1
tultraximo <jodeelmoody@gmail.com>
2024-09-20 12:46:07 -0700

    ape size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-1">
         {/* Logo  */}
         <Link href="/">
-          <span className="flex items-center space-x-2 text-xl font-medium text-black dark:text-gray-100">
+          <span className="flex items-center space-x-2 text-l font-medium text-black dark:text-gray-100">
               <span>
                 <Image
                   src="/img/whiteape.png"
@@ -32,7 +32,7 @@ export const Navbar = () => {
         </Link>
 
         {/* get started  */}
-        <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
+        <div >
             <ThemeChanger />
             {/* <div className="hidden mr-3 lg:flex nav__item">
               <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
